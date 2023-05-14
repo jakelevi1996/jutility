f19f22686e623919df0c6287d2ad544cd821abee
jakelevi1996 <jakelevi@hotmail.co.uk>
2023-05-14 18:24:16 +0100

    Add logo and script that makes it

---
+++ scripts/make_logo.py
@@ -0,0 +1,29 @@
+import os
+import numpy as np
+from jutility import plotting
+
+num_colours = 7
+cp = plotting.ColourPicker(num_colours, cyclic=False)
+x = np.linspace(-1, 7, 100)
+line_list = [
+    plotting.Line(
+        x=x,
+        y=((1 + (i/10)) * np.sin(x + (i / num_colours))),
+        c=cp(i),
+        label="Line %i" % i,
+    )
+    for i in range(num_colours)
+]
+mp = plotting.MultiPlot(
+    plotting.Subplot(
+        *line_list,
+        axis_properties=plotting.AxisProperties(axis_off=True, xlim=[-1, 7]),
+    ),
+    figure_properties=plotting.FigureProperties(
+        figsize=[10, 4],
+        title="  ".join("jutility"),
+        title_font_size=40,
+        top_space=0.2,
+    )
+)
+mp.save("logo", dir_name=os.path.join(os.getcwd(), "images"))

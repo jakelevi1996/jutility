3c496519011927c06c524f53c2ab97c289af55cd
jakelevi1996 <jakelevi@hotmail.co.uk>
2023-05-14 16:34:37 +0100

    Run unit tests with local copy of jutility package, not installed version

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,10 +1,10 @@
 import os
 import sys
 import numpy as np
-from jutility import util
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-SOURCE_DIR = os.path.abspath(os.path.join(CURRENT_DIR, ".."))
-sys.path.append(SOURCE_DIR)
+SOURCE_DIR = os.path.abspath(os.path.join(CURRENT_DIR, "..", "src"))
+sys.path.insert(0, SOURCE_DIR)
 
+from jutility import util
 util.numpy_set_print_options()

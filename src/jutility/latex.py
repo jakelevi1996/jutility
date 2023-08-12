439a76c23ab5f04f39d0d363a51a6e4f45592a93
jakelevi1996 <jakelevi@hotmail.co.uk>
2023-08-13 00:03:02 +0100

    Make initial indent configurable

--- src/jutility/latex.py
+++ src/jutility/latex.py
@@ -1,11 +1,11 @@
 from jutility import util
 
 class Indenter:
-    def __init__(self, indent_str=None):
+    def __init__(self, indent_str=None, initial_indent=0):
         if indent_str is None:
             indent_str = "".ljust(4)
         self._indent_str = indent_str
-        self._num_indent = 0
+        self._num_indent = initial_indent
 
         self.new_block = util.CallbackContext(
             lambda: self._add_indent( 1),

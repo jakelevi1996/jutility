c7e573df51d4e2e6ae1d8aedcd7dd801241b61da
jakelevi1996 <jakelevi@hotmail.co.uk>
2023-09-02 16:12:44 +0100

    Add `latex.Line` class

--- src/jutility/latex.py
+++ src/jutility/latex.py
@@ -39,10 +39,58 @@ def plot(
     with indent.new_block():
         for line in lines:
             line.plot(indent)
+            printer()
 
     printer("\\end{axis}")
     printer("\\end{tikzpicture}")
     printer("\\end{document}")
+
+class Line:
+    def __init__(
+        self,
+        x,
+        y,
+        c=None,
+        alpha=None,
+        marker=None,
+        label=None,
+        name=None,
+        column_width=25,
+    ):
+        self._x_list    = x
+        self._y_list    = y
+        self._colour    = c
+        self._alpha     = alpha
+        self._marker    = marker
+        self._label     = label
+        self._name      = name
+        self._width     = column_width
+
+    def plot(self, indent):
+        indent.print("\\addplot[")
+        with indent.new_block():
+            if self._colour is not None:
+                indent.print("color=%s," % self._colour)
+            if self._alpha is not None:
+                indent.print("opacity=%s," % self._alpha)
+            if self._marker is not None:
+                indent.print("mark=%s," % self._marker)
+            if self._name is not None:
+                indent.print("name path=%s," % self._name)
+            if self._label is None:
+                indent.print("forget plot,")
+
+        indent.print("]")
+        indent.print("table {")
+        with indent.new_block():
+            indent.print(format_table_row("x", "y", width=self._width))
+            for x, y in zip(self._x_list, self._y_list):
+                indent.print(format_table_row(x, y, width=self._width))
+
+        indent.print("};")
+
+        if self._label is not None:
+            indent.print("\\addlegendentry{%s}" % self._label)
 
 class AxisProperties:
     def __init__(
@@ -137,3 +185,7 @@ class Indenter:
 
     def print(self, s):
         self._print(self.indent(s))
+
+def format_table_row(*entries, width=0, sep=""):
+    row_str = sep.join(str(i).ljust(width) for i in entries)
+    return row_str

11699fd66bdbdb62418b616bea2d6685a309f95b
jakelevi1996 <jakelevi@hotmail.co.uk>
2023-09-02 16:17:22 +0100

    Add unit test for `latex.plot`

--- tests/test_latex.py
+++ tests/test_latex.py
@@ -1,6 +1,7 @@
+import os
 import numpy as np
 import pytest
-from jutility import util, latex
+from jutility import latex, plotting, util
 import tests.util
 
 OUTPUT_DIR = tests.util.get_output_dir("test_latex")
@@ -28,3 +29,28 @@ def test_indenter(indent_str, initial_indent):
             indent.print(next(x))
         indent.print(next(x))
     indent.print(next(x))
+
+def test_plot():
+    rng = util.Seeder().get_rng("test_plot")
+
+    x = np.linspace(0, 1, 100)
+    f = lambda x, slope: slope * x + 0.01 * rng.normal(size=x.shape)
+
+    n = 5
+    cp = plotting.ColourPicker(n)
+    lines = [
+        latex.Line(x, f(x, slope), c=cp.colour_name(i), label="Line %i" % i)
+        for i, slope in enumerate(np.linspace(0, 1, n))
+    ]
+
+    latex.plot(
+        *lines,
+        axis_properties=latex.AxisProperties(
+            title="Plot title",
+            xlabel="$x$ label",
+            ylabel="$y$ label",
+            colour_picker=cp,
+        ),
+        plot_name="test_plot_latex",
+        dir_name=os.path.join(OUTPUT_DIR, "test_plot")
+    )

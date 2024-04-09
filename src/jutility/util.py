d0454afbcb85b7f78bcdf57e010867c9f458cf7a
jakelevi1996 <jakelevi@hotmail.co.uk>
2024-04-09 17:37:39 +0100

    Use `start` argument to `enumerate`, rather than manually performing addition

--- src/jutility/util.py
+++ src/jutility/util.py
@@ -127,7 +127,7 @@ class Seeder:
         self._used_seeds = set()
 
     def get_seed(self, *args):
-        seed = sum((i + 1) * ord(c) for i, c in enumerate(str(args)))
+        seed = sum(i * ord(c) for i, c in enumerate(str(args), start=1))
         while seed in self._used_seeds:
             seed += 1
 

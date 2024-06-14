51ece456574f119a991b020310f0120c77792ddb
jakelevi1996 <jakelevi@hotmail.co.uk>
2024-06-14 13:54:43 +0100

    Remove `test_batch_first` test (no longer supported)

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -341,25 +341,6 @@ def test_quiver():
     mp.save(plot_name="test_quiver", dir_name=OUTPUT_DIR)
     assert os.path.isfile(mp.filename)
 
-def test_batch_first():
-    rng = util.Seeder().get_rng("test_batch_first")
-    nd = 100
-    batch_size = 5
-    x = np.linspace(0, 1, nd)
-    y = rng.normal(size=[batch_size, nd])
-    with pytest.raises(ValueError):
-        plotting.plot(
-            plotting.Line(x, y, c="b", alpha=0.6),
-            plot_name="test_batch_first",
-            dir_name=OUTPUT_DIR,
-        )
-
-    plotting.plot(
-        plotting.Line(x, y, c="b", alpha=0.6, batch_first=True),
-        plot_name="test_batch_first",
-        dir_name=OUTPUT_DIR,
-    )
-
 def test_text():
     plotting.plot(
         plotting.Text(0.5, 0.5, "Text example", size=60, center_align=True),

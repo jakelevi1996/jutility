3dc2b1cfd435ee36c598718ccb7a51ad2b010271
jakelevi1996 <jakelevi@hotmail.co.uk>
2024-07-16 16:16:42 +0100

    Add unit test `test_time_sweep`

---
+++ tests/test_time_sweep.py
@@ -0,0 +1,61 @@
+import os
+import time
+import numpy as np
+import pytest
+from jutility import util, plotting, time_sweep
+import tests
+
+OUTPUT_DIR = tests.get_output_dir("test_time_sweep")
+
+def test_time_sweep():
+    rng = util.Seeder().get_rng("test_time_sweep")
+
+    class Matmul(time_sweep.Experiment):
+        def __init__(self, rng):
+            self.rng = rng
+
+        def setup(self, n):
+            self.x1 = rng.normal(size=[n, n])
+            self.x2 = rng.normal(size=[n, n])
+
+        def run(self):
+            return self.x1 @ self.x2
+
+    class Solve(time_sweep.Experiment):
+        def __init__(self, rng):
+            self.rng = rng
+
+        def setup(self, n):
+            self.a = rng.normal(size=[n, n])
+            self.b = rng.normal(size=n)
+
+        def run(self):
+            return np.linalg.solve(self.a, self.b)
+
+    class LstSq(time_sweep.Experiment):
+        def __init__(self, rng):
+            self.rng = rng
+
+        def setup(self, n):
+            self.a = rng.normal(size=[n, n])
+            self.b = rng.normal(size=n)
+
+        def run(self):
+            return np.linalg.lstsq(self.a, self.b, rcond=None)
+
+    class Eig(time_sweep.Experiment):
+        def __init__(self, rng):
+            self.rng = rng
+
+        def setup(self, n):
+            self.x = rng.normal(size=[n, n])
+
+        def run(self):
+            return np.linalg.eig(self.x)
+
+    time_sweep.time_sweep(
+        *[t(rng) for t in [Matmul, Solve, LstSq, Eig]],
+        n_list=util.log_range(10, 100, 10),
+        dir_name=OUTPUT_DIR,
+    )
+

6ac11cdedca3b40cd7c7ea9489715047d7e69973
jakelevi1996 <jakelevi@hotmail.co.uk>
2024-07-17 19:20:11 +0100

    Add unit test for `cli.ObjectParser.parse_args`

---
+++ tests/test_cli.py
@@ -0,0 +1,47 @@
+import os
+import time
+import numpy as np
+import pytest
+from jutility import util, plotting, cli
+import tests
+
+OUTPUT_DIR = tests.get_output_dir("test_cli")
+
+def get_parser():
+    parser = cli.ObjectParser(
+        cli.Arg("int",   "in", default=10,            type=int),
+        cli.Arg("float", "fl", default=-4.7,          type=float),
+        cli.Arg("list",  "li", default=[30, 20, 10],  type=int, nargs="*"),
+        cli.Arg("none",  "no", default=None),
+        cli.Arg("true",  "tr", default=True),
+        cli.Arg("false", "fa", default=False),
+        cli.Arg("no_abbrev",   default="random"),
+        cli.ObjectArg(
+            Adam,
+            cli.Arg("lr",   "lr", type=float, default=1e-3),
+            cli.Arg("beta", "be", type=float, default=[0.9, 0.999], nargs=2),
+            abbreviation="op",
+            init_requires=["params"],
+        ),
+    )
+    return parser
+
+def test_parse():
+    printer = util.Printer("test_parse", OUTPUT_DIR)
+
+    parser = get_parser()
+    args = parser.parse_args([])
+
+    printer(parser)
+    printer(args)
+
+    assert parser.parse_args([]).int == 10
+    assert parser.parse_args([]).list == [30, 20, 10]
+    assert parser.parse_args(["--int=-3"]).int == -3
+    assert parser.parse_args(["--list", "3", "4", "5"]).list == [3, 4, 5]
+
+class Adam:
+    def __init__(self, params, lr=1e-3, beta=None):
+        self.params = params
+        self.lr     = lr
+        self.beta   = beta

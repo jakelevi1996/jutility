1a04ca9aab531f612331cfdc963ea686fbb729b3
jakelevi1996 <jakelevi@hotmail.co.uk>
2024-07-18 13:33:41 +0100

    Add unit test `test_init_nested_objects`

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -71,6 +71,10 @@ def test_init_object():
 
     parser = get_parser()
     parser.parse_args([])
+
+    with pytest.raises(ValueError):
+        optimiser = parser.init_object("Adam")
+
     optimiser = parser.init_object("Adam", params=[1, 2, 3])
 
     printer(optimiser, vars(optimiser))
@@ -86,8 +90,113 @@ def test_init_object():
     optimiser: Adam = parser.init_object("Adam", params=[20, 30])
     assert optimiser.inner_params == [20, 30]
 
+def get_nested_object_parser():
+    parser = cli.ObjectParser(
+        cli.Arg("num_epochs", "ne", type=int, default=10),
+        cli.ObjectArg(
+            Mlp,
+            cli.Arg("hidden_dim",           "hd", type=int, default=100),
+            cli.Arg("num_hidden_layers",    "nh", type=int, default=5),
+            cli.ObjectArg(
+                DeepSet,
+                cli.Arg("hidden_dim",           "hd", type=int, default=20),
+                cli.Arg("num_hidden_layers",    "nh", type=int, default=2),
+                name="encoder",
+                abbreviation="en",
+                init_requires=["input_dim", "output_dim"],
+                init_parsed_kwargs={"output_dim": "model.hidden_dim"},
+                init_const_kwargs={"input_dim": 4},
+            ),
+            name="model",
+            abbreviation="ml",
+            init_requires=["output_dim"],
+        ),
+    )
+    return parser
+
+def test_init_nested_objects():
+    printer = util.Printer("test_init_nested_objects", OUTPUT_DIR)
+
+    parser = get_nested_object_parser()
+    args = parser.parse_args([])
+    arg_dict: dict = vars(args)
+    printer(*arg_dict.items(), cli.get_args_summary(args), "-"*100, sep="\n")
+
+    assert args.num_epochs == 10
+    assert arg_dict["model.hidden_dim"] == 100
+    assert arg_dict["model.encoder.num_hidden_layers"] == 2
+    assert "model"          not in arg_dict
+    assert "model.encoder"  not in arg_dict
+
+    encoder = cli.init_object(args, "model.encoder")
+    printer(*arg_dict.items(), cli.get_args_summary(args), "-"*100, sep="\n")
+    assert "model.encoder" in arg_dict
+    assert "model" not in arg_dict
+    assert isinstance(encoder, DeepSet)
+    assert encoder.num_hidden_layers == 2
+    assert encoder.input_dim == 4
+    assert encoder.output_dim == arg_dict["model.hidden_dim"]
+
+    with pytest.raises(ValueError):
+        model = cli.init_object(args, "model")
+
+    model = cli.init_object(args, "model", output_dim=23)
+    printer(*arg_dict.items(), cli.get_args_summary(args), "-"*100, sep="\n")
+    assert "model"          in arg_dict
+    assert "model.encoder"  in arg_dict
+    assert isinstance(model, Mlp)
+    assert isinstance(model.encoder, DeepSet)
+    assert model.encoder is encoder
+    assert model.output_dim == 23
+    assert model.num_hidden_layers == 5
+    assert model.encoder.hidden_dim == 20
+
+    argv = ["--model.num_hidden_layers=10", "--model.encoder.hidden_dim=25"]
+    args = parser.parse_args(argv)
+    arg_dict: dict = vars(args)
+    printer(*arg_dict.items(), cli.get_args_summary(args), "-"*100, sep="\n")
+    assert "model"          not in arg_dict
+    assert "model.encoder"  not in arg_dict
+    model = cli.init_object(args, "model", output_dim=29)
+    assert "model"          in arg_dict
+    assert "model.encoder"  in arg_dict
+    assert isinstance(model, Mlp)
+    assert isinstance(model.encoder, DeepSet)
+    assert model.output_dim == 29
+    assert model.num_hidden_layers == 10
+    assert model.encoder.hidden_dim == 25
+
+    util.save_json(arg_dict, "test_init_nested_objects", OUTPUT_DIR)
+
 class Adam:
     def __init__(self, params, lr=1e-3, beta=None):
         self.inner_params   = params
         self.lr             = lr
         self.beta           = beta
+
+class _Model:
+    def __init__(
+        self,
+        output_dim,
+        hidden_dim,
+        num_hidden_layers,
+        input_dim=None,
+        encoder=None,
+    ):
+        self.input_dim          = input_dim
+        self.output_dim         = output_dim
+        self.hidden_dim         = hidden_dim
+        self.num_hidden_layers  = num_hidden_layers
+        self.encoder            = encoder
+
+class Mlp(_Model):
+    pass
+
+class Transformer(_Model):
+    pass
+
+class Cnn(_Model):
+    pass
+
+class DeepSet(_Model):
+    pass

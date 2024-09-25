4856f3f5f683f2034be6c69138fc5c34979fd711
jakelevi1996 <jakelevi@hotmail.co.uk>
2024-09-25 14:29:53 +0100

    Add `title_font_size` option to `AxisProperties`

--- src/jutility/plotting.py
+++ src/jutility/plotting.py
@@ -499,6 +499,7 @@ class AxisProperties:
         axis_off=False,
         grid=True,
         title=None,
+        title_font_size=12,
         title_colour=None,
         wrap_title=True,
         colour=None,
@@ -519,6 +520,7 @@ class AxisProperties:
         self._axis_off = axis_off
         self._grid = grid
         self._title = title
+        self._title_font_size = title_font_size
         self._title_colour = title_colour
         self._wrap_title = wrap_title
         self._colour = colour
@@ -559,7 +561,11 @@ class AxisProperties:
                 colour_dict["color"] = self._title_colour
             if self._wrap_title:
                 self._title = util.wrap_string(self._title)
-            axis.set_title(self._title, **colour_dict)
+            axis.set_title(
+                self._title,
+                fontsize=self._title_font_size,
+                **colour_dict,
+            )
         if self._colour is not None:
             axis.set_facecolor(self._colour)
 

7cdf6accc40c3b7816c4d7495231919f51f4c321
jakelevi1996 <jakelevi@hotmail.co.uk>
2024-12-31 16:12:01 +0000

    Add `scripts/cli_template.py` (copied from `README.md`)

---
+++ scripts/cli_template.py
@@ -0,0 +1,34 @@
+from jutility import plotting, util, cli
+
+class ExampleClass:
+    def __init__(self, p: float):
+        self.p = p
+
+def main(args: cli.Namespace):
+    x, y, z = args.get("x, y, z")
+    assert isinstance(x, float)
+    assert isinstance(y, str)
+    assert isinstance(z, bool)
+
+    with cli.verbose:
+        cn = cli.init_object(args, "ExampleClass")
+        assert isinstance(cn, ExampleClass)
+
+    print(x, y, z, cn.p, sep=", ")
+
+    ...
+
+if __name__ == "__main__":
+    parser = cli.ObjectParser(
+        cli.Arg("x", type=float, default=3.45),
+        cli.Arg("y", type=str,   default="\"default\""),
+        cli.Arg("z", action="store_true"),
+        cli.ObjectArg(
+            ExampleClass,
+            cli.Arg("p", type=float, default=6.78),
+        ),
+    )
+    args = parser.parse_args()
+
+    with util.Timer("main"):
+        main(args)

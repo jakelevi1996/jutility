4fa95909db07055b503337f9f8905b404b52776a
jakelevi1996 <jakelevi@hotmail.co.uk>
2025-03-12 20:55:01 +0000

    Remove `jutility.sweep` module (superceeded by `juml.train.Sweeper`)

--- src/jutility/__init__.py
+++ src/jutility/__init__.py
@@ -1,2 +1,8 @@
-from jutility import util, plotting, sweep, time_sweep, cli, units
-from jutility import _fix_matplotlib_constrained_layout
+from jutility import (
+    util,
+    plotting,
+    cli,
+    units,
+    time_sweep,
+    _fix_matplotlib_constrained_layout,
+)
